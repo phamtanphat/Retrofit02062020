062bd41a84d2b0c6661f2abda8e0125e1579edaf
phatdroid94 <phatdroid94@gmail.com>
2020-08-29 17:52:33 +0700

    retrofit create

--- app/src/main/java/com/example/retrofit02062020/MainActivity.java
+++ app/src/main/java/com/example/retrofit02062020/MainActivity.java
@@ -7,7 +7,14 @@ import android.os.Bundle;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.Retrofit.Builder;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -25,11 +32,19 @@ public class MainActivity extends AppCompatActivity {
 
         // Okhttp Client
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                
+                                .readTimeout(30 , TimeUnit.SECONDS)
+                                .writeTimeout(30 , TimeUnit.SECONDS)
+                                .connectTimeout(30 , TimeUnit.SECONDS)
+                                .retryOnConnectionFailure(true)
+                                .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                                .build();
+
+
 
         Retrofit retrofit = new Retrofit.Builder()
                             .baseUrl("https://khoapham.vn/")
                             .addConverterFactory(GsonConverterFactory.create(gson))
-                            .client()
+                            .client(okHttpClient)
+                            .build();
     }
 }
